3e67dd678b11f2a21e7a6473d752c37da3cc4f7f
Vaclav Rehak <vaclav@angelcam.com>
2015-11-26 15:55:00 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="logger",
+    version='1.0.0',
+    description="Angelcam Python logging helper",
+    keywords="logging loggly syslog",
+    author="Angelcam",
+    author_email="dev@angelcam.com",
+    url="https://bitbucket.org/angelcam/logger/",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "requests-futures >= 0.9.4"
+    ],
+    include_package_data=True,
+    platform='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers'
+    ]
+)
