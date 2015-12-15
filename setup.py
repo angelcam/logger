4f78f58b52d291aa53a74245e959e3f92f55caec
Martin Dobrovolny <martin@angelcam.com>
2015-12-15 14:40:39 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="logger",
-    version='1.0.0',
+    version='1.0.1',
     description="Angelcam Python logging helper",
     keywords="logging loggly syslog",
     author="Angelcam",
